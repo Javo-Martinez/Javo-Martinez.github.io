2f99521b405a82c5fb18d580c6fca6faeb6b70bc
Javo-Martinez <117385106+Javo-Martinez@users.noreply.github.com>
2026-08-21 20:14:03 -0300

    Update nota.js
    Ajustes búsqueda temas musicales

--- nota.js
+++ nota.js
@@ -383,7 +383,7 @@ async function mostrarNota(
     let contenidoHTML = await respuestaContenido.text();
 
     contenidoHTML = contenidoHTML.replace(
-                    /src="music\//g,
+                    /src="./musica\//g,
                     `src="${urlMusica}`
                     );
     
